06a379f07dcd2cb9254e7ee670586eb61c653d49
Sanjeev Kumar <sanjeev@sanjeev.(none)>
2014-02-02 23:51:42 +0000

    css and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Blog::Application.routes.draw do
+
+  root  'static_pages#home'
+
   resources :microposts
-
-
   resources :users
 
+  get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/home'
+
+  match '/help'  => 'static_pages#help'
+  match '/about' => 'static_pages#about'
+  match '/contact' => 'static_pages#contact'
+  match '/home' => 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
