b5aa9f11c16c6fb1f73e0bb22add6c98beb7c22a
Sanjeev Kumar <sanjeev@sanjeev.(none)>
2014-02-02 20:01:16 +0000

    fisrt push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0cb9cbcb9ec2cbb635749aa9937e388a459a3c85167c16e23a8846ca20f4adb9c796fc8d2d7f1648517b77403b3c0080b00a8b16b7e5406df46fd54a28748964'
